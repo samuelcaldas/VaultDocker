67fa7721050102a32e51c9746f900c300f4d5d30
Samuel Caldas <samuel.caldas@gmail.com>
2026-02-25 18:07:37 -0300

    feat: Update Nix channel and Node.js, add Python, LLDAP, and Copilot CLI packages, configure new environment variables and workspace hooks, and remove Firebase emulator setup.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,41 +1,54 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
+{ pkgs }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
+  channel = "stable-25.05"; # or "unstable"
+  # services.docker.enable = true;
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
-    pkgs.zulu
+    pkgs.nodejs_24
+    # pkgs.zulu
+    pkgs.python312
+    pkgs.python312Packages.pip
+    pkgs.uv
+    pkgs.lldap
+    pkgs.lldap-cli
+    pkgs.openssl
+    pkgs.github-copilot-cli
+    # pkgs.prisma
   ];
   # Sets environment variables in the workspace
-  env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
-  services.firebase.emulators = {
-    # Disabling because we are using prod backends right now
-    detect = false;
-    projectId = "demo-app";
-    services = ["auth" "firestore"];
+  env = {
+    CODEX_HOME = "${builtins.getEnv "HOME"}/studio/.codex";
+    XDG_CONFIG_HOME = "${builtins.getEnv "HOME"}/studio/.copilot";
+    DATABASE_URL = "file:./data/data.db";
   };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "ms-vscode.vscode-typescript-next"
+      "prisma.prisma"
+      "redhat.vscode-yaml"
+      "openai.chatgpt"
+      "google.geminicodeassist"
+      "redhat.vscode-xml"
+      "github.copilot"
     ];
     workspace = {
       onCreate = {
-        default.openFiles = [
-          "src/app/page.tsx"
-        ];
+        npm-install = "npm install -g @google/gemini-cli@latest @openai/codex@latest; exit;";
+      };
+      onStart = {
+        npm-install = "npm install -g @google/gemini-cli@latest @openai/codex@latest; exit;";
+        # ldap = "lldap run";
       };
     };
     # Enable previews and customize configuration
     previews = {
-      enable = true;
+      enable = false;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
           manager = "web";
         };
       };
